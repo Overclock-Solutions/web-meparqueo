3effa817a4da4bb91160e5c87cfee8cd28e53db3
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2025-03-23 12:35:41 -0500

    Eliminar archivos innecesarios y agregar contexto de socket y configuración de Redux

--- src/context/socket.ts
+++ src/context/socket.ts
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import React from "react";
 import io from "socket.io-client";
 export const socket = io(import.meta.env.VITE_SOCKET_URL);
-export const SocketContext = createContext();
+export const SocketContext = React.createContext(socket);
